5ced42956639cfd13ab4a5551174e1b00acd339b
Peter <murdzhevpetar@gmail.com>
2025-12-16 23:04:16 +0200

    fixed serverless function

--- api/gnews.js
+++ api/gnews.js
@@ -1,7 +1,7 @@
 
-export async function fetchNews(request, result) {
+export default async function fetchNews(req, res) {
     const apiKey = process.env.GNEWS_API_KEY;
-    const { query } = request.query;
+    const { query } = req.query;
 
 
     const url = `https://gnews.io/api/v4/top-headlines?topic=science&q=${query}&lang=en&max=10&token=${apiKey}`;
@@ -10,9 +10,9 @@ export async function fetchNews(request, result) {
         const response = await fetch(url);
         const data = await response.json();
 
-        result.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate");
-        result.status(200).json(data);
+        res.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate");
+        res.status(200).json(data);
     } catch (error) {
-        result.status(500).json({error : "Error fetching news"})
+        res.status(500).json({error : "Error fetching news"})
     }
 }
